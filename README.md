8761a90ea6a76bf7de95911c6e35345e8e388f7a
Jeff Larkin <jlarkin@nvidia.com>
2015-09-17 10:21:54 -0400

    Initial check-in with README and cg samples from lecture 2

---
+++ README.md
@@ -0,0 +1,6 @@
+NVIDIA OpenACC Course Sources and Labs
+======================================
+This repository contains sources used in the [NVIDIA OpenACC
+Course](https://developer.nvidia.com/openacc-course). The `lectures`
+subdirectory contains sources used in the presentation slides. The `labs`
+directory contains sources used in the homework labs.
---
+++ lectures/lecture2/cg-c99-parallel-loop/vector.h
@@ -0,0 +1,26 @@
+#pragma once
+#include<cmath>
+#include "matrix.h"
+
+struct vector {
+  unsigned int n;
+  double *coefs;
+};
+
+void allocate_vector(vector &v, unsigned int n) {
+  v.n=n;
+  v.coefs=(double*)malloc(n*sizeof(double));
+}
+
+void free_vector(vector &v) {
+  double *vcoefs=v.coefs;
+  free(v.coefs);
+
+}
+
+void initialize_vector(vector &v,double val) {
+
+  for(int i=0;i<v.n;i++)
+    v.coefs[i]=val;
+}
+
