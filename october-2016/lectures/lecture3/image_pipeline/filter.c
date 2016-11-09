c48ca31ab08821636bbf51a066832bd9f48cb3c3
Jeff Larkin <jlarkin@nvidia.com>
2016-11-09 10:27:32 -0500

    Added image filter pipeline example

---
+++ october-2016/lectures/lecture3/image_pipeline/filter.c
@@ -0,0 +1,229 @@
+/* Copyright (c) 2016, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of NVIDIA CORPORATION nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#define MAX(X,Y) ((X>Y) ? X:Y)
+#define MIN(X,Y) ((X<Y) ? X:Y)
+void blur5(unsigned restrict char *imgData, unsigned restrict char *out, long w, long h, long ch, long step)
+{
+  long x, y;
+  const int filtersize = 5;
+  double filter[5][5] = 
+  {
+     1,  1,  1,  1,  1,
+     1,  2,  2,  2,  1,
+     1,  2,  3,  2,  1,
+     1,  2,  2,  2,  1,
+     1,  1,  1,  1,  1
+  };
+  // The denominator for scale should be the sum
+  // of non-zero elements in the filter.
+  float scale = 1.0 / 35.0;
+   
+#pragma acc parallel loop collapse(2) gang vector copyin(imgData[0:w * h * ch]) copyout(out[0:w * h * ch])
+  for ( y = 0; y < h; y++ )
+  {
+    for ( x = 0; x < w; x++ )
+    {
+      float blue = 0.0, green = 0.0, red = 0.0;
+      for ( int fy = 0; fy < filtersize; fy++ )
+      {
+        long iy = y - (filtersize/2) + fy;
+        for ( int fx = 0; fx < filtersize; fx++ )
+        {
+          long ix = x - (filtersize/2) + fx;
+          if ( (iy<0)  || (ix<0) || 
+               (iy>=h) || (ix>=w) ) continue;
+          blue  += filter[fy][fx] * (float)imgData[iy * step + ix * ch];
+          green += filter[fy][fx] * (float)imgData[iy * step + ix * ch + 1];
+          red   += filter[fy][fx] * (float)imgData[iy * step + ix * ch + 2];
+        }
+      }
+      out[y * step + x * ch]      = 255 - (scale * blue);
+      out[y * step + x * ch + 1 ] = 255 - (scale * green);
+      out[y * step + x * ch + 2 ] = 255 - (scale * red);
+    }
+  }
+}
+void blur5_blocked(unsigned restrict char *imgData, unsigned restrict char *out, long w, long h, long ch, long step)
+{
+  long x, y;
+  const int filtersize = 5, nblocks = 8;
+  double filter[5][5] = 
+  {
+     1,  1,  1,  1,  1,
+     1,  2,  2,  2,  1,
+     1,  2,  3,  2,  1,
+     1,  2,  2,  2,  1,
+     1,  1,  1,  1,  1
+  };
+  // The denominator for scale should be the sum
+  // of non-zero elements in the filter.
+  float scale = 1.0 / 35.0;
+   
+  long blocksize = h/ nblocks;
+#pragma acc data copyin(imgData[:w*h*ch],filter)copyout(out[:w*h*ch])
+  for ( long blocky = 0; blocky < nblocks; blocky++)
+  {
+    // For data copies we need to include the ghost zones for the filter
+    long starty = blocky * blocksize;
+    long endy   = starty + blocksize;
+#pragma acc parallel loop collapse(2) gang vector
+    for ( y = starty; y < endy; y++ )
+    {
+      for ( x = 0; x < w; x++ )
+      {
+        float blue = 0.0, green = 0.0, red = 0.0;
+        for ( int fy = 0; fy < filtersize; fy++ )
+        {
+          long iy = y - (filtersize/2) + fy;
+          for ( int fx = 0; fx < filtersize; fx++ )
+          {
+            long ix = x - (filtersize/2) + fx;
+            if ( (iy<0)  || (ix<0) || 
+                (iy>=h) || (ix>=w) ) continue;
+            blue  += filter[fy][fx] * (float)imgData[iy * step + ix * ch];
+            green += filter[fy][fx] * (float)imgData[iy * step + ix * ch + 1];
+            red   += filter[fy][fx] * (float)imgData[iy * step + ix * ch + 2];
+          }
+        }
+        out[y * step + x * ch]      = 255 - (scale * blue);
+        out[y * step + x * ch + 1 ] = 255 - (scale * green);
+        out[y * step + x * ch + 2 ] = 255 - (scale * red);
+      }
+    }
+  }
+}
+void blur5_update(unsigned restrict char *imgData, unsigned restrict char *out, long w, long h, long ch, long step)
+{
+  long x, y;
+  const int filtersize = 5, nblocks = 8;
+  double filter[5][5] = 
+  {
+     1,  1,  1,  1,  1,
+     1,  2,  2,  2,  1,
+     1,  2,  3,  2,  1,
+     1,  2,  2,  2,  1,
+     1,  1,  1,  1,  1
+  };
+  // The denominator for scale should be the sum
+  // of non-zero elements in the filter.
+  float scale = 1.0 / 35.0;
+   
+  long blocksize = h/ nblocks;
+#pragma acc data create(imgData[w*h*ch],out[w*h*ch]) copyin(filter)
+  {
+  for ( long blocky = 0; blocky < nblocks; blocky++)
+  {
+    // For data copies we need to include the ghost zones for the filter
+    long starty = MAX(0,blocky * blocksize - filtersize/2);
+    long endy   = MIN(h,starty + blocksize + filtersize/2);
+#pragma acc update device(imgData[starty*step:blocksize*step])
+    starty = blocky * blocksize;
+    endy = starty + blocksize;
+#pragma acc parallel loop collapse(2) gang vector
+    for ( y = starty; y < endy; y++ )
+    {
+      for ( x = 0; x < w; x++ )
+      {
+        float blue = 0.0, green = 0.0, red = 0.0;
+        for ( int fy = 0; fy < filtersize; fy++ )
+        {
+          long iy = y - (filtersize/2) + fy;
+          for ( int fx = 0; fx < filtersize; fx++ )
+          {
+            long ix = x - (filtersize/2) + fx;
+            if ( (iy<0)  || (ix<0) || 
+                (iy>=h) || (ix>=w) ) continue;
+            blue  += filter[fy][fx] * (float)imgData[iy * step + ix * ch];
+            green += filter[fy][fx] * (float)imgData[iy * step + ix * ch + 1];
+            red   += filter[fy][fx] * (float)imgData[iy * step + ix * ch + 2];
+          }
+        }
+        out[y * step + x * ch]      = 255 - (scale * blue);
+        out[y * step + x * ch + 1 ] = 255 - (scale * green);
+        out[y * step + x * ch + 2 ] = 255 - (scale * red);
+      }
+    }
+#pragma acc update self(out[starty*step:blocksize*step])
+  }
+  }
+}
+void blur5_pipelined(unsigned restrict char *imgData, unsigned restrict char *out, long w, long h, long ch, long step)
+{
+  long x, y;
+  const int filtersize = 5, nblocks = 8;
+  double filter[5][5] = 
+  {
+     1,  1,  1,  1,  1,
+     1,  2,  2,  2,  1,
+     1,  2,  3,  2,  1,
+     1,  2,  2,  2,  1,
+     1,  1,  1,  1,  1
+  };
+  // The denominator for scale should be the sum
+  // of non-zero elements in the filter.
+  float scale = 1.0 / 35.0;
+   
+  long blocksize = h/ nblocks;
+#pragma acc data create(imgData[w*h*ch],out[w*h*ch])
+  {
+  for ( long blocky = 0; blocky < nblocks; blocky++)
+  {
+    // For data copies we need to include the ghost zones for the filter
+    long starty = MAX(0,blocky * blocksize - filtersize/2);
+    long endy   = MIN(h,starty + blocksize + filtersize/2);
+#pragma acc update device(imgData[starty*step:blocksize*step]) async(blocky%3+1)
+    starty = blocky * blocksize;
+    endy = starty + blocksize;
+#pragma acc parallel loop collapse(2) gang vector  async(blocky%3+1)
+    for ( y = starty; y < endy; y++ )
+    {
+      for ( x = 0; x < w; x++ )
+      {
+        float blue = 0.0, green = 0.0, red = 0.0;
+        for ( int fy = 0; fy < filtersize; fy++ )
+        {
+          long iy = y - (filtersize/2) + fy;
+          for ( int fx = 0; fx < filtersize; fx++ )
+          {
+            long ix = x - (filtersize/2) + fx;
+            if ( (iy<0)  || (ix<0) || 
+                (iy>=h) || (ix>=w) ) continue;
+            blue  += filter[fy][fx] * (float)imgData[iy * step + ix * ch];
+            green += filter[fy][fx] * (float)imgData[iy * step + ix * ch + 1];
+            red   += filter[fy][fx] * (float)imgData[iy * step + ix * ch + 2];
+          }
+        }
+        out[y * step + x * ch]      = 255 - (scale * blue);
+        out[y * step + x * ch + 1 ] = 255 - (scale * green);
+        out[y * step + x * ch + 2 ] = 255 - (scale * red);
+      }
+    }
+#pragma acc update self(out[starty*step:blocksize*step]) async(blocky%3+1)
+  }
+#pragma acc wait
+  }
+}
---
+++ october-2016/lectures/lecture3/image_pipeline/main.cpp
@@ -0,0 +1,97 @@
+/* Copyright (c) 2016, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of NVIDIA CORPORATION nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <strings.h>
+#include <opencv/cv.h>
+#include <opencv/cvaux.h>
+#include <opencv/highgui.h>
+#include <omp.h>
+
+extern "C" {
+void blur5(unsigned char*,unsigned char*,long,long,long,long);
+void blur5_blocked(unsigned char*,unsigned char*,long,long,long,long);
+void blur5_update(unsigned char*,unsigned char*,long,long,long,long);
+void blur5_pipelined(unsigned char*,unsigned char*,long,long,long,long);
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc < 3)
+  {
+    fprintf(stderr,"Usage: %s inFilename outFilename\n",argv[0]);
+    return -1;
+  }
+
+  IplImage* img = cvLoadImage(argv[1]);
+
+  printf("%s: %d x %d, %d %d\n", argv[1],img->width, img->height, img->widthStep, img->nChannels);
+
+  unsigned char *data = (unsigned char*)img->imageData;
+  long width = img->width,
+       height = img->height,
+       ch = img->nChannels,
+       ws = img->widthStep,
+       sz = height * ch * ws;
+  unsigned char *out = (unsigned char*)malloc(sz * sizeof(unsigned char));
+
+  // Pre-allocate device and queues for timing
+  blur5(data,out,width,height, ch, ws);
+  blur5_pipelined(data,out,width,height, ch, ws);
+  bzero(out,sz);
+
+  double st = omp_get_wtime();
+  blur5(data,out,width,height, ch, ws);
+  double et = omp_get_wtime();
+  bzero(out,sz);
+  printf("Time (original): %lf seconds\n", (et-st));
+
+  st = omp_get_wtime();
+  blur5_blocked(data,out,width,height, ch, ws);
+  et = omp_get_wtime();
+  bzero(out,sz);
+  printf("Time (blocked): %lf seconds\n", (et-st));
+
+  st = omp_get_wtime();
+  blur5_update(data,out,width,height, ch, ws);
+  et = omp_get_wtime();
+  bzero(out,sz);
+  printf("Time (update): %lf seconds\n", (et-st));
+
+  st = omp_get_wtime();
+  blur5_pipelined(data,out,width,height, ch, ws);
+  et = omp_get_wtime();
+  printf("Time (pipelined): %lf seconds\n", (et-st));
+  memcpy(img->imageData,out,width*height*ch);
+
+  if(!cvSaveImage(argv[2],img))
+    fprintf(stderr,"Failed to write to %s.\n",argv[2]);
+
+  cvReleaseImage(&img);
+
+  return 0;
+}
